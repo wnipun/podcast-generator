24a3a93efc65dfb717101fe5752d932e984c8611
wnipun <3061001+wnipun@users.noreply.github.com>
2025-05-12 22:06:07 +0000

    Add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "==============="
+git config --global user.name "${GITHUB_ACTION}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
+echo "==============="
